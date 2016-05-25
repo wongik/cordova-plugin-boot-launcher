af0bbf8ea7d1852ce31a7df2e34a71d954e5aac9
wongik <eric.wong@asahiarts.com>
2016-05-25 16:13:32 +0800

    Update BootLauncher.java

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -8,7 +8,7 @@ public class BootLauncher extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent serviceIntent = new Intent(context, net.test.app.class);
+        Intent serviceIntent = new Intent(context, net.test.app.CordovaApp.class);
         serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(serviceIntent);
     }
